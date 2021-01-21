306d8e5f853b49fff1ba3d75bced734d33ff9ba5
yehudis raitman <yehudisr@gmail.com>
2021-01-21 16:46:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,7 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
- pigeon_data = {}
- new_array = data.values
- new_array.collect do |index|
+ pigeon_data = data.eachcollect do |index|
   new_array[index]
    
    binding.pry
