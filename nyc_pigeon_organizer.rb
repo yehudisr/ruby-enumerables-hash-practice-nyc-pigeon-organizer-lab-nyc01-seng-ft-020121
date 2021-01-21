81897be1c37b53f8d54267dc157f0a136661908d
yehudis raitman <yehudisr@gmail.com>
2021-01-21 17:43:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
  data.each_with_object({}) do |(key, value), pigeon_data|
     value.each_value {|new_value| new_value} 
     
-    pigeon_data[key] =
+    pigeon_data[key] = new_value
    
    
   
