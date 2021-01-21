3f029a2d050649d728adbf7584951fd01cc4d60b
yehudis raitman <yehudisr@gmail.com>
2021-01-21 17:44:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,14 +3,13 @@ require "pry"
 def nyc_pigeon_organizer(data)
  
  data.each_with_object({}) do |(key, value), pigeon_data|
+    
     value.each_value {|new_value| new_value} 
     
-    pigeon_data[key] = new_value
-   
    
   
    binding.pry
    
    end
- 
+  pigeon_data[key] = new_value
 end
