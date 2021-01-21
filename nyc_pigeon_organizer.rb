3d298b3e0508c17fd6df44946f0dcc821d64e618
yehudis raitman <yehudisr@gmail.com>
2021-01-21 21:19:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,14 @@ def nyc_pigeon_organizer(data)
  data.each_with_object({}) do |(key, value), final_hash|
   
    value.each do | inner_key, names|
+     
      names.each do |name|
+      
       if !final_hash[name]
       final_hash[name] = {}
-       binding.pry 
+       
+      binding.pry 
+     
      end
      end
      end 
