5e4cef9113caf8087732f7e7e9e50d27796058de
yehudis raitman <yehudisr@gmail.com>
2021-01-21 16:48:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,7 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
- pigeon_data = data.keys.each_with_object({}) do |index|
-  new_array[index]
-   
+ pigeon_data = data.keys.each_with_object({}) { |k,h| h[k] = controller.controller_name.include?(k) ? 'active' : '' }
    binding.pry
- end
+ 
 end
