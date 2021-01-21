77a76d153b161fa885d6fda81293b5a70f94f391
yehudis raitman <yehudisr@gmail.com>
2021-01-21 21:28:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,8 +11,6 @@ def nyc_pigeon_organizer(data)
       if !final_hash[name]
       final_hash[name] = {}
       
-      if data[value] == name 
-      final_hash[]  
         
       binding.pry 
     end
