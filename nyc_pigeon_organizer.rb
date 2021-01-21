64078e7d29a9359d92019e498715bc8307252412
yehudis raitman <yehudisr@gmail.com>
2021-01-21 21:07:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,12 @@ require "pry"
 def nyc_pigeon_organizer(data)
  
  data.each_with_object({}) do |(key, value), final_hash|
-    new_key = 
+  
    value.each do | inner_key, names|
-     new_key = names
+     names
     binding.pry 
      end 
-   final_hash[new_key]
+   final_hash[names]
   binding.pry
     puts "hello"
    end
