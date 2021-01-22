bd332d5ae7122b22620f1f2262076cbafa3a76c9
yehudis raitman <yehudisr@gmail.com>
2021-01-22 02:46:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ final_hash = {}
+ data.each do |key, value|
+  
+   value.each do |inner_key, names|
+     
+     names.each do |name|
+      
+      if !final_hash[name]
+      final_hash[name] = {}
+      end 
+       
+      if !final_hash[name][key]
+      !final_hash[name][key] = [] 
+      end
+     
+      final_hash[name][key] << inner_key.to_s
+    
+    end
+    end
+  end 
+  final_hash
 end
+
